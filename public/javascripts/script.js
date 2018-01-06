0b10047d3953921368a2a205d611a8bb6bc54271
babylee2002 <babylee2002@gmail.com>
2018-01-06 18:48:05 -0500

    working on the timer

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,34 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
-$("#timer").html("Hello")
+    let counter = 0
+    let timeLeft = parseInt(($('#waitTime').html())*60)
+    console.log($('#waitTime').html())
+    console.log(timeLeft)
+    function convertSeconds(s) {
+        var min = (Math.floor(s / 60)).toString()
+        var sec = s % 60
+        if (min.length < 2) {
+            // min = parseInt(`${0 + min}`)
+            return '0' + min + ':' + sec
+        }
+    }
+    const timer = $('#timer')
+    timer.html(counter)
+    var audio = new Audio('/audio/ding.mp3')
 
+    function timeIt() {
+        counter++
+        timer.html(convertSeconds(timeLeft - counter))
+        if (counter === timeLeft) {
+            $('#timer').html('Your food is ready!')
+            audio.play()
+            audio.loop = true
+        }
+    }
 
-
-
-
-
-
+    setInterval(timeIt, 1000)
+    console.log(timeLeft)
+    $('#pause').on('click', () => { audio.pause() })
 
 
 
